aee364c523f33e09b002a82a0d78eb87f312ff4b
David Hallinan & Brandi M. Ford <pair+BUMBALORD.lilpaw@devbootcamp.com>
2015-04-09 11:03:33 -0700

    add users controller

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -5,7 +5,16 @@ end
 
 #user sign up
 get '/users/new' do
+  erb :signup
 end
 
 post '/users' do
+  @user = User.new(params)
+  if @user.save
+    status 200
+    redirect '/'
+  else
+    status 400
+    redirect '/users/new'
+  end
 end
