5dc770defac4d55b338623ea509a8288bcbc788b
David Hallinan & Brandi M. Ford <pair+BUMBALORD.lilpaw@devbootcamp.com>
2015-04-09 10:58:13 -0700

    add sessions controller

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,9 +3,19 @@ get '/sessions/new' do
 end
 
 post '/sessions' do
+  @user = User.where(username: params[:username]).first
 
+  if @user && (@user.password == params[:password])
+    session[:user_id] = @user.id
+    status 200
+    redirect '/'
+  else
+    status 400
+    redirect '/sessions/new'
+  end
 end
 
 delete '/sessions/:id' do
-
+  session.delete(:user_id)
+  redirect '/'
 end
