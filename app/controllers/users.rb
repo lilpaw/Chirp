a842e27793d35eba0eaf31f8ac4770224d7e8465
David Hallinan & Brandi M. Ford <pair+BUMBALORD.lilpaw@devbootcamp.com>
2015-04-10 15:47:29 -0700

    fix page flow, add signup/logout buttons

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,6 +1,25 @@
 #user homepage, include login and sign up
 get '/' do
-  erb :index
+  erb :"user/login"
+end
+
+post '/sessions' do
+  @user = User.where(username: params[:username]).first
+
+  if @user && (@user.password == params[:password])
+    session[:user_id] = @user.id
+    status 200
+    redirect "/users/#{current_user.id}/profile"
+  else
+    status 400
+    @login_failed = true
+    erb :"user/login"
+  end
+end
+
+delete '/sessions/:id' do
+  session.delete(:user_id)
+  redirect '/'
 end
 
 #user sign up
@@ -17,4 +36,11 @@ post '/users' do
     status 400
     redirect '/users/new'
   end
+end
+
+# display current user tweets aka profile page
+get '/users/:id/profile' do
+  @user_tweets = Tweet.where(user_id: params[:id])
+  erb :"user/profile"
+
 end
